62b6866e42dda50fab99be3568b33b03f8c32691
acorrgom <a.corrales.2020@alumnos.urjc.es>
2020-10-06 18:08:28 +0200

    Añadiendo funcionalidad lsa

--- build_features.py
+++ build_features.py
@@ -1,5 +1,16 @@
+import joblib
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.pipeline import Pipeline
+
+
 def build_tfidf(ctx):
     pass
 
+
 def build_lsa(ctx):
-    pass
+    text_pipeline = Pipeline([('vect', CountVectorizer()),
+                              ('tfidf', TfidfTransformer()),
+                              ('svd', TruncatedSVD())])
+
+    joblib.dump(text_pipeline, ctx.obj['pipeline_output_file'])
