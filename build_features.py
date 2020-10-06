65a2e10bf6337a9cb702c82f3d54e718eec460ef
acorrgom <a.corrales.2020@alumnos.urjc.es>
2020-10-06 18:21:35 +0200

    Se han añadido cambio build features Usuario 3

--- build_features.py
+++ build_features.py
@@ -3,3 +3,27 @@ def build_tfidf(ctx):
 
 def build_lsa(ctx):
     pass
+import click
+
+@click.command()
+@click.pass_context
+def build_tfidf(ctx):
+    pass
+
+@click.command()
+@click.pass_context
+def build_lsa(ctx):
+    pass
+
+@click.group()
+@click.argument('pipeline_output_file',
+                type=click.Path(exists=False, dir_okay=False))
+@click.pass_context
+def entry_point(ctx, pipeline_output_file):
+    ctx.obj['pipeline_output_file'] = pipeline_output_file
+
+
+if __name__ == '__main__':
+    entry_point.add_command(build_tfidf)
+    entry_point.add_command(build_lsa)
+    entry_point(obj={})
