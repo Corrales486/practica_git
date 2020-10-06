26176910aa298bbc77a6486bedd6926116110fb7
acorrgom <a.corrales.2020@alumnos.urjc.es>
2020-10-06 23:13:27 +0200

    Se ha vuelto al anterior commit y se ha añadido otro parametro random_state

--- build_features.py
+++ build_features.py
@@ -21,7 +21,7 @@ def build_lsa(ctx):
 
     text_pipeline = Pipeline([('vect', CountVectorizer()),
                               ('tfidf', TfidfTransformer()),
-                              ('truncated_svd', TruncatedSVD(n_components = 100))])
+                              ('truncated_svd', TruncatedSVD(n_components = 100, random_state=42))])
 
     joblib.dump(text_pipeline, ctx.obj['pipeline_output_file'])
 
