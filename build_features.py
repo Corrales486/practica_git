d2f03ad29f2b8895a507f3c4789442cef23b7501
acorrgom <a.corrales.2020@alumnos.urjc.es>
2020-10-06 18:06:53 +0200

    Se añade nuevo codigo de definicion de funciones

--- build_features.py
+++ build_features.py
@@ -1,5 +1,14 @@
+import joblib
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+
+
 def build_tfidf(ctx):
-    pass
+    text_pipeline = Pipeline([('vect', CountVectorizer()),
+                              ('tfidf', TfidfTransformer())])
+
+    joblib.dump(text_pipeline, ctx.obj['pipeline_output_file'])
+
 
 def build_lsa(ctx):
     pass
